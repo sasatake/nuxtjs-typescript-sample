b5f7714cbe81c4a07f62f036ca5039e43e63861d
akira satake <t06418as@gmail.com>
2019-04-11 22:44:17 +0900

    add input form dialog

--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,78 @@
 <template>
-  <v-layout column justify-center align-center>
-    <v-card>
-      <v-card-title primary-title>
-        <div class="headline">User List</div>
-      </v-card-title>
-      <v-data-table :headers="headers" :items="user.users" hide-actions>
-        <template v-slot:items="props">
-          <td>
-            <v-avatar size="36px">
-              <img :src="props.item.avatar" />
-            </v-avatar>
-          </td>
-          <td>{{ props.item.name }}</td>
-          <td>{{ props.item.email }}</td>
-          <td>{{ props.item.city }}</td>
-          <td>{{ props.item.updatedAt | dateTime }}</td>
-          <td>
-            <v-icon small @click="alertUser(props.item.name)">
-              edit
-            </v-icon>
-          </td>
-          <td>
-            <v-icon small @click="deleteUser(props.item.name)">
-              delete
-            </v-icon>
-          </td>
-        </template>
-      </v-data-table>
-      <v-btn absolute dark fab top right color="pink">
-        <v-icon>add</v-icon>
-      </v-btn>
-    </v-card>
-  </v-layout>
+  <v-container>
+    <v-layout column justify-center align-center>
+      <v-card>
+        <v-card-title primary-title>
+          <div class="headline">User List</div>
+        </v-card-title>
+        <v-data-table :headers="headers" :items="user.users" hide-actions>
+          <template v-slot:items="props">
+            <td>
+              <v-avatar size="36px">
+                <img :src="props.item.avatar" />
+              </v-avatar>
+            </td>
+            <td>{{ props.item.name }}</td>
+            <td>{{ props.item.email }}</td>
+            <td>{{ props.item.city }}</td>
+            <td>{{ props.item.updatedAt | dateTime }}</td>
+            <td>
+              <v-icon small @click="alertUser(props.item.name)">
+                edit
+              </v-icon>
+            </td>
+            <td>
+              <v-icon small @click="deleteUser(props.item.name)">
+                delete
+              </v-icon>
+            </td>
+          </template>
+        </v-data-table>
+        <v-btn
+          absolute
+          dark
+          fab
+          top
+          right
+          color="pink"
+          @click.stop="dialog = true"
+        >
+          <v-icon>add</v-icon>
+        </v-btn>
+      </v-card>
+    </v-layout>
+    <v-layout>
+      <v-dialog v-model="dialog" persistent max-width="50%">
+        <v-card>
+          <v-card-title class="headline">Create User</v-card-title>
+          <v-card-text>
+            <v-container grid-list-xs>
+              <v-layout wrap>
+                <v-flex xs6>
+                  <v-text-field label="First Name" required></v-text-field>
+                </v-flex>
+                <v-flex xs6>
+                  <v-text-field label="Last Name" required></v-text-field>
+                </v-flex>
+                <v-flex xs12>
+                  <v-text-field label="Email" required></v-text-field>
+                </v-flex>
+                <v-flex xs12>
+                  <v-select :items="cities" label="City"></v-select>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="green darken-1" flat="flat" @click="dialog = false">
+              Create
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-layout>
+  </v-container>
 </template>
 
 <script lang="ts">
@@ -76,6 +117,10 @@ export default class Index extends Vue {
       value: 'delete'
     }
   ];
+
+  dialog: boolean = false;
+
+  cities: Array<string> = ['Tokyo', 'Osaka', 'New York', 'London'];
 
   @State user!: UserState;
 
