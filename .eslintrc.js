021a5295bce031bdbe68f1964bcb197380eaffac
akira satake <t06418as@gmail.com>
2019-04-14 12:23:05 +0900

    overrule dot-notation

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,7 @@ module.exports = {
   // add your custom rules here
   rules: {
     'nuxt/no-cjs-in-config': 'off',
-    '@typescript-eslint/no-unused-vars': 'error'
+    '@typescript-eslint/no-unused-vars': 'error',
+    'dot-notation': 'off'
   }
 };
